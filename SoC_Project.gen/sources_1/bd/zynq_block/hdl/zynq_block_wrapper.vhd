217df10e454a273cb7cf3bbab6e910e5951354d9
Benjamin <benjamin.car@tuwien.ac.at>
2023-04-05 21:49:27 +0200

    - commit for dynamic reconfiguration

--- SoC_Project.gen/sources_1/bd/zynq_block/hdl/zynq_block_wrapper.vhd
+++ SoC_Project.gen/sources_1/bd/zynq_block/hdl/zynq_block_wrapper.vhd
@@ -2,7 +2,7 @@
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
 --Date        : Mon Mar  6 19:36:12 2023
---Host        : running 64-bit major release  (build 9200)
+--Host        : PC running 64-bit major release  (build 9200)
 --Command     : generate_target zynq_block_wrapper.bd
 --Design      : zynq_block_wrapper
 --Purpose     : IP block netlist
